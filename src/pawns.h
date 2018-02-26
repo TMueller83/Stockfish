9393c20b966233f7826a33cfa006c8d6d883d56e
IIvec <ivan.ivec@gmail.com>
2018-02-26 17:16:39 +0100

    Cleaning

--- src/pawns.h
+++ src/pawns.h
@@ -38,7 +38,7 @@ struct Entry {
   Bitboard passed_pawns(Color c) const { return passedPawns[c]; }
   Bitboard pawn_attacks_span(Color c) const { return pawnAttacksSpan[c]; }
   int weak_unopposed(Color c) const { return weakUnopposed[c]; }
-  int pawn_initiative() const { return initiative; }
+  int pawn_asymmetry() const { return asymmetry; }
   int open_files() const { return openFiles; }
 
   int semiopen_file(Color c, File f) const {
@@ -72,7 +72,7 @@ struct Entry {
   int castlingRights[COLOR_NB];
   int semiopenFiles[COLOR_NB];
   int pawnsOnSquares[COLOR_NB][COLOR_NB]; // [color][light/dark squares]
-  int initiative;
+  int asymmetry;
   int openFiles;
 };
 
