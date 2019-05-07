1938af2771c1c1bb1562afd89f341d70f142b7b8
MichaelB7 <michaelbyrne1978@gmail.com>
2019-05-07 13:58:55 -0400

    Initalize Honey 1.0

--- src/misc.cpp
+++ src/misc.cpp
@@ -1,15 +1,15 @@
 /*
-  SugaR, a UCI chess playing engine derived from Stockfish
+  Honey, a UCI chess playing engine derived from Stockfish and SugaR
   Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
   Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
   Copyright (C) 2015-2019 Marco Costalba, Joona Kiiski, Gary Linscott, Tord Romstad
 
-  SugaR is free software: you can redistribute it and/or modify
+  Honey is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  SugaR is distributed in the hope that it will be useful,
+  Honey is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
@@ -56,7 +56,7 @@ namespace {
 
 /// Version number. If Version is left empty, then compile date in the format
 /// DD-MM-YY and show in engine_info.
-const string Version = "1.6.1";
+const string Version = "1.0";
 
 /// Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 /// cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
@@ -116,9 +116,9 @@ public:
 
 } // namespace
 
-/// engine_info() returns the full name of the current SugaR version. This
-/// will be either "SugaR <Tag> DD-MM-YY" (where DD-MM-YY is the date when
-/// the program was compiled) or "SugaR <Version>", depending on whether
+/// engine_info() returns the full name of the current Honey version. This
+/// will be either "Honey <Tag> DD-MM-YY" (where DD-MM-YY is the date when
+/// the program was compiled) or "Honey <Version>", depending on whether
 /// Version is empty.
 
 const string engine_info(bool to_uci) {
@@ -127,7 +127,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "SugaR " << Version << setfill('0');
+  ss << "Honey " << Version << setfill('0');
 
   if (Version.empty())
   {
@@ -135,10 +135,8 @@ const string engine_info(bool to_uci) {
       ss << setw(2) << day << setw(2) << (1 + months.find(month) / 4) << year.substr(2);
   }
 
-  ss << (Is64Bit ? " 64" : "")
-     << (HasPext ? " BMI2" : (HasPopCnt ? " POPCNT" : ""))
-     << (to_uci  ? "\nid author ": " by ")
-     << "Stockfish Team, Marco Zerbinati, Sergey Aleksandrovitch Kozlov";
+  ss << (to_uci  ? "\nid author ": " by ")
+     << "M. Byrne and scores of others";
 
   return ss.str();
 }
