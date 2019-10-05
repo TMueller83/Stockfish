9f3103c49098c83538cf00b3cbd7f5c9327a3bd7
VoyagerOne <excelgeek@gmail.com>
2019-10-05 10:42:36 -0400

    Introduce separate counter-move tables for captures
    Enhance counter-move history table by adding a capture/no-capture dimension,
    depending wether the previous move was a quiet move or a capture. This doubles
    the size of the table but provides more accurate move ordering.
    
    STC:
    LLR: 2.95 (-2.94,2.94) [0.50,4.50]
    Total: 79702 W: 17720 L: 17164 D: 44818
    http://tests.stockfishchess.org/tests/view/5d97945e0ebc590c21aa724b
    
    LTC:
    LLR: 2.96 (-2.94,2.94) [0.00,3.50]
    Total: 29147 W: 4907 L: 4651 D: 19589
    http://tests.stockfishchess.org/tests/view/5d97ccb90ebc590c21aa7bc0
    
    Closes https://github.com/official-stockfish/Stockfish/pull/2344
    
    Bench: 4131643

--- src/search.cpp
+++ src/search.cpp
@@ -537,8 +537,9 @@ void Thread::search() {
   std::memset(ss-7, 0, 10 * sizeof(Stack));
   for (int i = 7; i > 0; i--)
 
-     (ss-i)->continuationHistory = &this->continuationHistory[NO_PIECE][0]; // Use as sentinel
-ss->pv = pv;
+      (ss-i)->continuationHistory = &this->continuationHistory[0][NO_PIECE][0]; // Use as a sentinel
+
+  ss->pv = pv;
 
   bestValue = delta = alpha = -VALUE_INFINITE;
   size_t multiPV = Options["MultiPV"];
@@ -848,12 +849,13 @@ namespace {
 #endif
     bool captureOrPromotion, doFullDepthSearch, moveCountPruning, ttCapture;
 
-    Piece movedPiece;
+    Piece movedPiece, priorCapture;
     int moveCount, captureCount, quietCount, singularLMR;
 
     // Step 1. Initialize node
     Thread* thisThread = pos.this_thread();
     inCheck = pos.checkers();
+    priorCapture = pos.captured_piece();
     Color us = pos.side_to_move();
     moveCount = captureCount = quietCount = singularLMR = ss->moveCount = 0;
     bestValue = -VALUE_INFINITE;
@@ -978,7 +980,7 @@ if (   Threads.stop.load(std::memory_order_relaxed) || ss->ply >= MAX_PLY)
                     update_quiet_stats(pos, ss, ttMove, nullptr, 0, stat_bonus(depth));
 
                 // Extra penalty for early quiet moves of the previous ply
-                if ((ss-1)->moveCount <= 2 && !pos.captured_piece())
+                if ((ss-1)->moveCount <= 2 && !priorCapture)
                     update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + 1));
             }
             // Penalty for a quiet ttMove that fails low
@@ -1147,7 +1149,7 @@ if (   Threads.stop.load(std::memory_order_relaxed) || ss->ply >= MAX_PLY)
         Depth R = (835 + 70 * depth) / 256 + std::min(int(eval - beta) / 185, 3);
 
         ss->currentMove = MOVE_NULL;
-        ss->continuationHistory = &thisThread->continuationHistory[NO_PIECE][0];
+        ss->continuationHistory = &thisThread->continuationHistory[0][NO_PIECE][0];
         pos.do_null_move(st);
         Value nullValue = -search<NonPV>(pos, ss+1, -beta, -beta+1, depth-R, !cutNode);
         pos.undo_null_move();
@@ -1198,7 +1200,7 @@ if (   Threads.stop.load(std::memory_order_relaxed) || ss->ply >= MAX_PLY)
                 probCutCount++;
 
                 ss->currentMove = move;
-                ss->continuationHistory = &thisThread->continuationHistory[pos.moved_piece(move)][to_sq(move)];
+                ss->continuationHistory = &thisThread->continuationHistory[!!priorCapture][pos.moved_piece(move)][to_sq(move)];
 
                 assert(depth >= 5);
 
@@ -1436,7 +1438,7 @@ moves_loop: // When in check, search starts from here
 
       // Update the current move (this must be done after singular extension search)
       ss->currentMove = move;
-      ss->continuationHistory = &thisThread->continuationHistory[movedPiece][to_sq(move)];
+      ss->continuationHistory = &thisThread->continuationHistory[!!priorCapture][movedPiece][to_sq(move)];
 
       // Step 15. Make the move
       pos.do_move(move, st, givesCheck);
@@ -1664,13 +1666,13 @@ moves_loop: // When in check, search starts from here
 
         // Extra penalty for a quiet TT or main killer move in previous ply when it gets refuted
         if (   ((ss-1)->moveCount == 1 || ((ss-1)->currentMove == (ss-1)->killers[0]))
-            && !pos.captured_piece())
+            && !priorCapture)
                 update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -stat_bonus(depth + 1));
 
     }
     // Bonus for prior countermove that caused the fail low
     else if (   (depth >= 3 || PvNode)
-             && !pos.captured_piece())
+             && !priorCapture)
         update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, stat_bonus(depth));
 
     if (PvNode)
@@ -1717,6 +1719,7 @@ moves_loop: // When in check, search starts from here
     Move ttMove, move, bestMove;
     Depth ttDepth;
     Value bestValue, value, ttValue, futilityValue, futilityBase, oldAlpha;
+    Piece priorCapture;
 #ifdef Fortress
     bool ttHit, pvHit, inCheck, givesCheck, evasionPrunable, gameCycle;
 #else
@@ -1735,6 +1738,7 @@ moves_loop: // When in check, search starts from here
     (ss+1)->ply = ss->ply + 1;
     bestMove = MOVE_NONE;
     inCheck = pos.checkers();
+    priorCapture = pos.captured_piece();
     moveCount = 0;
 	  
 #ifdef Fortress
@@ -1907,7 +1911,7 @@ moves_loop: // When in check, search starts from here
       }
 
       ss->currentMove = move;
-      ss->continuationHistory = &thisThread->continuationHistory[pos.moved_piece(move)][to_sq(move)];
+      ss->continuationHistory = &thisThread->continuationHistory[!!priorCapture][pos.moved_piece(move)][to_sq(move)];
 
       // Make and search the move
       pos.do_move(move, st, givesCheck);
