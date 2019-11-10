1f5bd76975b1eab1f5d6a27de213616ec6ed868e
MichaelB7 <michaelbyrne1978@gmail.com>
2019-11-09 23:21:07 -0500

    Speculative try
    Bench: 5075511

--- src/search.cpp
+++ src/search.cpp
@@ -1013,7 +1013,7 @@ moves_loop: // When in check, search starts from here
       newDepth = depth - 1 + extension;
 
       // Step 14. Pruning at shallow depth (~170 Elo)
-      if (  !rootNode
+      if (  !PvNode
           && pos.non_pawn_material(us)
           && bestValue > VALUE_MATED_IN_MAX_PLY)
       {
